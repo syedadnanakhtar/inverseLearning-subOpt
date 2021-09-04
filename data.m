3d31b5319a774157fd3fe904858943f2fbaa7fc6
Syed Adnan Akhtar <adnan.wasazra@gmail.com>
2021-09-04 14:16:06 +0200

    removed some getter functions

--- data.m
+++ data.m
@@ -10,37 +10,29 @@ classdef data < handle
         nFeatures
         nStates%number of states x
         nActions%number of actions u 
-        goalPose%Joint angles at the goal issues
-        goalObj%3D coordinates of the goal obj
-        end_eff%3D coordinates of the end eff
-        startidx
-        endidx
     end
 
     methods
         %Contructor
-        function obj = data(a,startidx,endidx)
-            obj.filename = a;
-            q = read_motionFile(a);
-            obj.x =q.data(1:end-1,[21:28]);
-            obj.u = q.data(2:end,21:28) - q.data(1:end-1,21:28);
-            if startidx ~= -1
-                obj.x = obj.x(startidx:end,:);
-                obj.u = obj.u(startidx:end,:);
-            end
-            if endidx ~= -1
-                obj.x = obj.x(1:endidx,:);
-                obj.u = obj.u(1:endidx,:);
-            end        
- 
+        function obj = data(a)
+            obj.filename = a;    
         end        
+        
         %Function to construct features
-        function [obj] = constructFeatures(obj)
+        function [obj] = constructFeatures(obj,a)
             %function to construct features
             n = size(obj.featureList,2);
-            obj.phi = [];
-            for i=1:n
-                obj.phi = [obj.phi eval(obj.featureList{i})];
+            switch nargin
+                case 1
+                    obj.phi = [];
+                    for i=1:n
+                        obj.phi = [obj.phi eval(obj.featureList{i})];
+                    end
+                otherwise
+                    obj.phi = [];
+                    for i=1:n
+                        obj.phi = [obj.phi eval(obj.featureList{i})];
+                    end
             end
         end
         
@@ -54,15 +46,6 @@ classdef data < handle
     end
         methods   
         %Getter functions
-        function val = get.goalObj(obj)
-            q = read_motionFile(obj.filename);
-            idx = find(strcmp(q.labels,'goal_tx'));
-            val = q.data(50,idx:idx+2);
-
-        end
-        function val = get.goalPose(obj)
-            val = obj.x(end,:);   
-        end
         function val = get.length(obj)
             val = size(obj.x,1);
         end
@@ -74,11 +57,6 @@ classdef data < handle
         end
         function val = get.nActions(obj)
             val = size(obj.u,2);
-        end
-        function val = get.end_eff(obj)
-            q = read_motionFile(obj.filename);
-            idx = find(strcmp(q.labels,'end_eff_tx'));
-            val = q.data(1:obj.length,idx:idx+2);
-        end      
+        end    
     end
 end
